229bec29f147e7619f4cd0fd8a015dd258e64f0f
alkore <martinebelo@gmail.com>
2025-12-11 18:29:45 +0100

    Update selfutil.h

--- selfutil_patched/selfutil.h
+++ selfutil_patched/selfutil.h
@@ -1,41 +1,10 @@
 #pragma once
-
-#include <vector>
 #include <string>
-#include <cstdio>
-#include <filesystem>
+#include <vector>
 #include "compat/elf.h"
 #include "self.h"
 
 using namespace std;
-
-// --- Compat macOS pour ELF ---
-#ifndef EI_CLASS
-#define EI_CLASS      4
-#define EI_DATA       5
-#define EI_VERSION    6
-#define EI_OSABI      7
-#endif
-
-#ifndef ELFCLASS64
-#define ELFCLASS64    2
-#endif
-
-#ifndef ELFDATA2LSB
-#define ELFDATA2LSB   1
-#endif
-
-#ifndef EV_CURRENT
-#define EV_CURRENT    1
-#endif
-
-#ifndef ELFOSABI_FREEBSD
-#define ELFOSABI_FREEBSD 9
-#endif
-
-#ifndef ELF_MAGIC
-#define ELF_MAGIC 0x464C457F
-#endif
 
 class SelfUtil
 {
@@ -44,22 +13,22 @@ class SelfUtil
     Self_Hdr* seHead;
     vector<Self_Entry*> entries;
 
-    elf64_hdr* eHead;
-    unat elfHOffs;
+    Elf64_Ehdr* eHead;
+    size_t elfHOffs;
 
     vector<Elf64_Phdr*> phdrs;
 
 public:
     SelfUtil() {}
 
-    SelfUtil(string filePath)
-    {
+    // Constructeur unique pour éviter double définition
+    explicit SelfUtil(const string& filePath) {
         if(!Load(filePath))
             printf("Error, Load() failed!\n");
     }
 
     bool Parse();
     bool TestIdent();
-    bool Load(string filePath);
-    bool SaveToELF(string savePath);
+    bool Load(const string& filePath);
+    bool SaveToELF(const string& savePath);
 };
