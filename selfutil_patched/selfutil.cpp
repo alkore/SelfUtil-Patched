bcd4ea4637299da17272d7bde6eb9f342159d17c
alkore <martinebelo@gmail.com>
2025-12-11 18:06:52 +0100

    Update selfutil.cpp

--- selfutil_patched/selfutil.cpp
+++ selfutil_patched/selfutil.cpp
@@ -6,7 +6,8 @@
 
 using namespace std;
 
-// ------------------- ELF definitions -------------------
+// ------------------- Type definitions -------------------
+using u16 = uint16_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
 
