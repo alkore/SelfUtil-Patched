9b644623902412d06e9adc8a4af2f1172a6d0bbd
alkore <martinebelo@gmail.com>
2025-12-11 18:39:37 +0100

    Update self.h

--- selfutil_patched/self.h
+++ selfutil_patched/self.h
@@ -1,22 +1,25 @@
 #pragma once
+#include <cstddef> // size_t
 #include <cstdint>
+#include <vector>
 
-typedef uint8_t  u8;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef size_t   unat;
+typedef size_t unat;
 
+// SELF constants
 #define PS4_SELF_MAGIC 0x53454C46
-#define PS5_SELF_MAGIC 0x53454C46
+#define PS5_SELF_MAGIC 0x50534546
+#define PS4_PAGE_SIZE 0x1000
+#define PS5_PAGE_SIZE 0x1000
 
 struct Self_Hdr {
     u32 magic;
     u32 num_entries;
+    // autres champs si nécessaire
 };
 
 struct Self_Entry {
-    u32 props;
     u64 offs;
     u64 fileSz;
     u64 memSz;
+    u32 props;
 };
