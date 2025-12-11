05a1dfb6317a6a53b492d16e4b1dff9ac8db0b4c
alkore <martinebelo@gmail.com>
2025-12-11 18:37:11 +0100

    Update selfutil.h

--- selfutil_patched/selfutil.h
+++ selfutil_patched/selfutil.h
@@ -1,36 +1,26 @@
 #pragma once
-
 #include "elf.h"
 #include "self.h"
-#include <string>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 class SelfUtil
 {
     vector<u8> data, save;
-
     Self_Hdr* seHead;
     vector<Self_Entry*> entries;
-
     elf64_hdr* eHead;
     size_t elfHOffs;
-
     vector<Elf64_Phdr*> phdrs;
 
 public:
     SelfUtil() {}
-
-    SelfUtil(const string& filePath)
-    {
-        if (!Load(filePath))
-            printf("Error, Load() failed!\n");
-    }
+    SelfUtil(const string& filePath) { if (!Load(filePath)) printf("Error, Load() failed!\n"); }
 
     bool Parse();
     bool TestIdent();
-
     bool Load(const string& filePath);
     bool SaveToELF(const string& savePath);
 };
