834a238ca1b29351a523397ca5ce40d2cfa578ea
alkore <martinebelo@gmail.com>
2025-12-11 17:47:38 +0100

    Update selfutil.h

--- selfutil_patched/selfutil.h
+++ selfutil_patched/selfutil.h
@@ -1,33 +1,51 @@
 #pragma once
 
-#include "compat/elf.h"
-#include "self.h"
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <filesystem>
+#include "compat/elf.h"  // <-- inclus avant tout pour les typedef et macros ELF
 
 using namespace std;
 
-class SelfUtil
-{
-    vector<u8> data, save;
+typedef unsigned char u8;
+typedef unsigned int u32;
+typedef unsigned long long unat;
 
-    Self_Hdr* seHead;
-    vector<Self_Entry*> entries;
+constexpr u32 ELF_MAGIC = 0x464C457F; // 0x7F 'E' 'L' 'F'
+// PT_SCE_VERSION vient déjà de compat/elf.h
 
-    Elf64_Ehdr* eHead;
-    unat elfHOffs;
+void print_usage();
 
-    vector<Elf64_Phdr*> phdrs;
+extern string input_file_path;
+extern string output_file_path;
+extern bool dry_run;
+extern bool verbose;
+extern bool overwrite;
+extern bool align_size;
+extern bool patch_first_segment_duplicate;
+extern Elf64_Off patch_first_segment_safety_percentage;
+extern bool patch_version_segment;
 
+extern int first_min_offset;
+
+bool compare_u8_array(u8* first_array, u8* second_array, int size);
+void set_u8_array(u8* first_array, int value, int size);
+
+class SelfUtil {
 public:
-    SelfUtil() {}
-
-    SelfUtil(string filePath)
-    {
-        if (!Load(filePath))
-            printf("Error, Load() failed!\n");
-    }
-
-    bool Parse();
-    bool TestIdent();
+    SelfUtil(string filePath) { Load(filePath); }
     bool Load(string filePath);
     bool SaveToELF(string savePath);
+    bool Parse();
+    bool TestIdent();
+
+private:
+    vector<u8> data;
+    vector<u8> save;
+    Self_Hdr* seHead;
+    vector<Self_Entry*> entries;
+    elf64_hdr* eHead;
+    vector<Elf64_Phdr*> phdrs;
+    Elf64_Off elfHOffs;
 };
