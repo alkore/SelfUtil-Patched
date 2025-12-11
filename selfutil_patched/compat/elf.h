c6c182d5b66d6af24c1d241ac0f4aee695c02b3b
alkore <martinebelo@gmail.com>
2025-12-11 17:34:21 +0100

    Update elf.h

--- selfutil_patched/compat/elf.h
+++ selfutil_patched/compat/elf.h
@@ -1,8 +1,10 @@
-#ifndef COMPAT_ELF_H
-#define COMPAT_ELF_H
+#pragma once
 
-#include <stdint.h>
+#include <cstdint>
 
+// -----------------------------------------------------------------------------
+// Basic ELF types
+// -----------------------------------------------------------------------------
 typedef uint16_t Elf64_Half;
 typedef uint32_t Elf64_Word;
 typedef int32_t  Elf64_Sword;
@@ -10,11 +12,14 @@ typedef uint64_t Elf64_Xword;
 typedef int64_t  Elf64_Sxword;
 typedef uint64_t Elf64_Addr;
 typedef uint64_t Elf64_Off;
+typedef uint16_t Elf64_Section;
+typedef uint16_t Elf64_Versym;
 
-#define EI_NIDENT 16
-
+// -----------------------------------------------------------------------------
+// ELF Header
+// -----------------------------------------------------------------------------
 typedef struct {
-    unsigned char e_ident[EI_NIDENT];
+    unsigned char e_ident[16];
     Elf64_Half    e_type;
     Elf64_Half    e_machine;
     Elf64_Word    e_version;
@@ -30,6 +35,9 @@ typedef struct {
     Elf64_Half    e_shstrndx;
 } Elf64_Ehdr;
 
+// -----------------------------------------------------------------------------
+// Program Header
+// -----------------------------------------------------------------------------
 typedef struct {
     Elf64_Word p_type;
     Elf64_Word p_flags;
@@ -41,10 +49,9 @@ typedef struct {
     Elf64_Xword p_align;
 } Elf64_Phdr;
 
-#ifndef PT_LOOS
-#define PT_LOOS    0x60000000
-#endif
-
+// -----------------------------------------------------------------------------
+// Constants
+// -----------------------------------------------------------------------------
 #ifndef PT_SCE_VERSION
 #define PT_SCE_VERSION (PT_LOOS + 0xfffff01)  // .sce_version
 #endif
@@ -53,4 +60,12 @@ typedef struct {
 #define ELFCLASS64 2
 #endif
 
-#endif // COMPAT_ELF_H
+#define ELF_MAGIC 0x464C457F  // 0x7F 'E' 'L' 'F'
+#define EV_CURRENT 1
+#define EI_CLASS 4
+#define EI_DATA 5
+#define EI_VERSION 6
+#define EI_OSABI 7
+#define ELFDATA2LSB 1
+#define ELFOSABI_FREEBSD 9
+#define EM_X86_64 62
