36f10819a5f99853a33f8eb64f5d936005f38c47
alkore <martinebelo@gmail.com>
2025-12-11 17:33:59 +0100

    Update selfutil.h

--- selfutil_patched/selfutil.h
+++ selfutil_patched/selfutil.h
@@ -1,22 +1,28 @@
 #pragma once
 
-#include "compat/elf.h"
-#include "self.h"
 #include <vector>
 #include <string>
+#include <cstdint>
 #include <cstdio>
+#include "compat/elf.h"
+#include "self.h"
 
 using namespace std;
 
+// Typedefs
+typedef uint8_t  u8;
+typedef uint64_t u64;
+typedef uint64_t unat;
+
 class SelfUtil
 {
-    vector<uint8_t> data, save;
+    vector<u8> data, save;
 
     Self_Hdr* seHead;
     vector<Self_Entry*> entries;
 
-    Elf64_Ehdr* eHead;   // anciennement elf64_hdr*
-    uint64_t elfHOffs;
+    Elf64_Ehdr* eHead;
+    unat elfHOffs;
 
     vector<Elf64_Phdr*> phdrs;
 
@@ -31,7 +37,6 @@ public:
 
     bool Parse();
     bool TestIdent();
-
     bool Load(string filePath);
     bool SaveToELF(string savePath);
 };
