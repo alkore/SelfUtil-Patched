bc9fae9d12c0c693841c3f167c0a04404bb47bcf
alkore <martinebelo@gmail.com>
2025-12-11 18:02:10 +0100

    Update elf.h

--- selfutil_patched/compat/elf.h
+++ selfutil_patched/compat/elf.h
@@ -1,10 +1,7 @@
 #pragma once
-
 #include <cstdint>
 
-// -----------------------------------------------------------------------------
-// Basic ELF types
-// -----------------------------------------------------------------------------
+// Types ELF standard
 typedef uint16_t Elf64_Half;
 typedef uint32_t Elf64_Word;
 typedef int32_t  Elf64_Sword;
@@ -15,29 +12,25 @@ typedef uint64_t Elf64_Off;
 typedef uint16_t Elf64_Section;
 typedef uint16_t Elf64_Versym;
 
-// -----------------------------------------------------------------------------
-// ELF Header
-// -----------------------------------------------------------------------------
+// ELF header
 typedef struct {
     unsigned char e_ident[16];
-    Elf64_Half    e_type;
-    Elf64_Half    e_machine;
-    Elf64_Word    e_version;
-    Elf64_Addr    e_entry;
-    Elf64_Off     e_phoff;
-    Elf64_Off     e_shoff;
-    Elf64_Word    e_flags;
-    Elf64_Half    e_ehsize;
-    Elf64_Half    e_phentsize;
-    Elf64_Half    e_phnum;
-    Elf64_Half    e_shentsize;
-    Elf64_Half    e_shnum;
-    Elf64_Half    e_shstrndx;
+    Elf64_Half e_type;
+    Elf64_Half e_machine;
+    Elf64_Word e_version;
+    Elf64_Addr e_entry;
+    Elf64_Off  e_phoff;
+    Elf64_Off  e_shoff;
+    Elf64_Word e_flags;
+    Elf64_Half e_ehsize;
+    Elf64_Half e_phentsize;
+    Elf64_Half e_phnum;
+    Elf64_Half e_shentsize;
+    Elf64_Half e_shnum;
+    Elf64_Half e_shstrndx;
 } Elf64_Ehdr;
 
-// -----------------------------------------------------------------------------
-// Program Header
-// -----------------------------------------------------------------------------
+// Program header
 typedef struct {
     Elf64_Word p_type;
     Elf64_Word p_flags;
@@ -49,23 +42,22 @@ typedef struct {
     Elf64_Xword p_align;
 } Elf64_Phdr;
 
-// -----------------------------------------------------------------------------
-// Constants
-// -----------------------------------------------------------------------------
-#ifndef PT_SCE_VERSION
-#define PT_SCE_VERSION (PT_LOOS + 0xfffff01)  // .sce_version
-#endif
+// Segment types
+#define PT_NULL    0
+#define PT_LOAD    1
+#define PT_DYNAMIC 2
+#define PT_INTERP  3
+#define PT_NOTE    4
+#define PT_SHLIB   5
+#define PT_PHDR    6
+#define PT_TLS     7
+#define PT_LOOS    0x60000000
+#define PT_HIOS    0x6fffffff
+#define PT_LOPROC  0x70000000
+#define PT_HIPROC  0x7fffffff
 
-#ifndef ELFCLASS64
-#define ELFCLASS64 2
-#endif
+// PS4/PS5 specific
+#define PT_SCE_VERSION (PT_LOOS + 0xfffff01)
 
-#define ELF_MAGIC 0x464C457F  // 0x7F 'E' 'L' 'F'
-#define EV_CURRENT 1
-#define EI_CLASS 4
-#define EI_DATA 5
-#define EI_VERSION 6
-#define EI_OSABI 7
-#define ELFDATA2LSB 1
-#define ELFOSABI_FREEBSD 9
-#define EM_X86_64 62
+// ELF magic
+#define ELF_MAGIC 0x464C457F
